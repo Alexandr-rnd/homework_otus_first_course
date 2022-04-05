dd75191d0c132b644f1b5d496ccaf488077c392b
Александр Мойсиевич <yan.pys61@gmail.com>
2022-04-05 20:59:29 +0300

    added conftest.py + venv

--- conftest.py
+++ conftest.py
@@ -1,30 +1,29 @@
-# import pytest
-# from src.Triangle import Triangle
-# from src.Square import Square
-# from src.Rectangle import Rectangle
-# from src.Circle import Circle
-# from src.Figure import Figure
-# #
-# def create_correct_figure():
-#     triangle = Triangle(10,15,20)
-#     square = Square(30)
-#     circle = Circle(5)
-#     rectangle = Rectangle(10,20)
-#     return triangle, square, circle, rectangle
-#
-# def delete_correct_figure():
-#     triangle = None
-#     square = None
-#     circle = None
-#     rectangle = None
-#     print("was deleted")
-#     return triangle, square, circle, rectangle
-#
-#
-# @pytest.fixture()
-# def create_and_clear_figure():
-#     create = create_correct_figure()
-#     yield create
-#     delete_correct_figure()
-#
-#
+import pytest
+from src.Triangle import Triangle
+from src.Square import Square
+from src.Rectangle import Rectangle
+from src.Circle import Circle
+
+
+@pytest.fixture()
+def create_triangle():
+    triangle_test = Triangle(side_a=5, side_b=5, side_c=5)
+    return triangle_test
+
+
+@pytest.fixture()
+def create_circle():
+    circle_test = Circle(side_a=10)
+    return circle_test
+
+
+@pytest.fixture()
+def create_rectangle():
+    rectangle_test = Rectangle(side_a=5, side_b=10)
+    return rectangle_test
+
+
+@pytest.fixture()
+def create_square():
+    square_test = Square(side_a=10)
+    return square_test
